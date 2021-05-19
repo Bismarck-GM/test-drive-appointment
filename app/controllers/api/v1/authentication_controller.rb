d0b2920bd62fc1b96f4ebd2281de32b988299df9
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-19 02:07:23 -0300

    Add basic token generation for login.

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
       class AuthenticateError < StandardError; end
 
@@ -8,12 +9,28 @@ module Api
       rescue_from AuthenticateError, with: :handle_unauthenticated
 
       def create
-        if user
-          raise AuthenticateError unless user.authenticate(params.require(:password))
-          render json: UserRepresenter.new(user).as_json, status: :created
+        user = User.find_by(username: params[:username])
+        if !user
+          render json: { error: "Invalid username"}, status: :unauthorized
         else
-          render json: { error: 'No such user' }, status: :unauthorized
+          if user.authenticate(params[:password])
+            secret_key = Rails.application.secrets.secret_key_base[0]
+            token = JWT.encode({
+              user_id: user.id,
+              username: user.username,
+            }, secret_key)
+            render json: { token: token }
+          else
+            render json: { message: "Wrong Password!"}, status: :unauthorized
+          end
         end
+
+        # if user
+        #   raise AuthenticateError unless user.authenticate(params.require(:password))
+        #   render json: user, status: :created
+        # else
+        #   render json: { error: 'No such user' }, status: :unauthorized
+        # end
       end
 
       private

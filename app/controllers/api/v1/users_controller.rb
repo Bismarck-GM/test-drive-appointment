138ead129068d8b0b9fc211a2808af5ac19452ec
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-19 00:36:11 -0300

    Add simple user creation Controller, Route, password with bcrypt.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,24 @@
-class UsersController < ApplicationController
+module Api
+  module V1
+    class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      def create
+        user = User.create(
+          username: params[:username],
+          password: params[:password],
+        )
+          if user.save
+          render json: user, status: :created
+        else
+          render json: { error: user.errors.full_messages.first, user: user, params: params }, status: :unprocessable_entity
+        end
+      end
+      private
+
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
+
+    end
+  end
 end

75696e5e66ba11372a3ab0827c13f880d0e4aac9
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-20 03:12:47 -0300

    Add logic to work with admin roles. Fix some issues with JWT service.

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,20 +2,27 @@ module Api
   module V1
     class AuthenticationController < ApplicationController
 
-      class AuthenticateError < StandardError; end
-      rescue_from AuthenticateError, with: :handle_unauthenticated
-
       def create
         if !user
           handle_unauthenticated
         else
-          if user.authenticate(auth_params[:password])
-            secret_key = Rails.application.secrets.secret_key_base[0]
-            token = JWT.encode({
-              user_id: user.id,
+          if user.authenticate(auth_params[:password]) && user.admin?
+            token = AuthenticationTokenService.encode(user)
+            render json: {
+              logged_in: true,
               username: user.username,
-            }, secret_key)
-            render json: { token: token }
+              email: user.email,
+              admin: user.admin,
+              token: token,
+            }
+          elsif user.authenticate(auth_params[:password])
+            token = AuthenticationTokenService.encode(user)
+            render json: {
+              logged_in: true,
+              username: user.username,
+              email: user.email,
+              token: token,
+            }
           else
             handle_unauthenticated
           end
@@ -25,15 +32,15 @@ module Api
       private
 
       def user
-        @user ||= User.find_by(username: auth_params[:username])
+        @user ||= User.find_by(username: auth_params[:username]) || User.find_by(email: auth_params[:email])
       end
 
       def handle_unauthenticated
-        render json: { error: 'Incorrect password or username.' }, status: :unauthorized
+        render json: { error: 'Incorrect password, username or e-mail.' }, status: :unauthorized
       end
 
       def auth_params
-        params.require(:authentication).permit(:username, :password)
+        params.require(:authentication).permit(:username, :password, :email, :admin)
       end
 
     end
--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,17 +1,18 @@
 class AuthenticationTokenService
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = ENV['JWT_SECRET']
   ALGORITHM_TYPE = 'HS256'.freeze
 
-  def self.call(user_id)
+  def self.encode(user)
     exp = 24.hours.from_now.to_i
-    payload = { user_id: user_id, exp: exp }
+    payload = { username: user.username, user_id: user.id, exp: exp }
     JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 
   def self.decode(token)
-    JWT.decode token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE }
+    decoded_token = JWT.decode token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE }
+    message = { user_id: decoded_token[0]["user_id"] }
     rescue JWT::ExpiredSignature, JWT::DecodeError
-    false
+      expired_token
   end
 
   def self.valid_payload(payload)
@@ -23,6 +24,6 @@ class AuthenticationTokenService
   end
 
   def self.expired_token
-    render json: { error: 'Expired token! login again' }, status: :unauthorized
+    message = { error: "Expired Token or Decoding Error. Please log-in again." }
   end
 end

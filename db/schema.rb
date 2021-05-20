712fec8826814ee2dc917bf4caf21541c8f1cb16
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-20 03:14:19 -0300

    Add GET route to Users for Admin. Edit User Migration, add Boolean field for Admin role.

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_222456) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
+    t.boolean "admin", default: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

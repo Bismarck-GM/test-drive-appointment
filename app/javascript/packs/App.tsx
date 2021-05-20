fe31cef2a9f79841eba2ed960effb413262ba04b
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-05-20 10:11:18 -0300

    Add containers and basic layout. Might split into Back-end and front-end projects...

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -1,74 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { Route, Switch } from 'react-router';
-// import { ConnectedRouter } from 'connected-react-router';
-
-// import configureStore, { history } from './store/index';
-// import { INITIAL_STATE } from './store/root';
-
-// import Index from './routes/index';
-// import Pokemon from './routes/pokemon';
-
-// import Header from './components/header';
-
-// import utilStyles from './utility.module.css';
-
-// const store = configureStore(INITIAL_STATE);
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Nav from '../containers/Nav';
 
 const App = () => {
-  // <Provider store={store}>
-  //   <ConnectedRouter history={history}>
-  //     <Header />
-  //     <div className={utilStyles.wideBannerSection} />
-  //     <Switch>
-  //       <Route exact path="/" component={Index} />
-  //       <Route exact path="/pokemon/:id" component={Pokemon} />
-  //     </Switch>
-  //   </ConnectedRouter>
-  // </Provider>
-  const data = {
-    user:
-    {
-      username: 'somebody6999',
-      password: 'somepassword',
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
     },
-  };
-  const user = fetch('/api/v1/register', {
-    method: 'Post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then(res => res.json())
-    .then(login => console.log(login));
-  console.log(user);
+  });
 
   return (
-    <div>
-      Hola vite, se limo Rails o webpack
-      <ul>
-        <li>
-          Esto parece que funca
-        </li>
-        <img src="https://cache.bmwusa.com/cosy.arox?pov=walkaround&brand=WBBM&vehicle=21XB&client=byoc&paint=P0B39&fabric=FKCSW&sa=S01SQ,S0249,S02TB,S0302,S0313,S0316,S0319,S0322,S0337,S03AG,S03DZ,S03MC,S0402,S0420,S0423,S0430,S0431,S0473,S0481,S0493,S04WF,S0508,S0534,S0544,S0563,S05A1,S05AS,S0676,S06AC,S06AK,S06UN,S0710,S0715,S0760,S0775&resp=webp&quality=50&BKGND=TRANSPARENT&angle=60&width=750" alt="asd" />
-        <li>
-          bien ahora aparentemente...
-        </li>
-      </ul>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <div style={{ display: 'flex' }}>
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <h1>Hello</h1>
+            </Route>
+          </Switch>
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 };
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.body.appendChild(document.createElement('div')),
-  );
-});
 
 export default App;
-
+// eslint-disable-next-line max-len
+// <img src="https://cache.bmwusa.com/cosy.arox?pov=walkaround&brand=WBBM&vehicle=21XB&client=byoc&paint=P0B39&fabric=FKCSW&sa=S01SQ,S0249,S02TB,S0302,S0313,S0316,S0319,S0322,S0337,S03AG,S03DZ,S03MC,S0402,S0420,S0423,S0430,S0431,S0473,S0481,S0493,S04WF,S0508,S0534,S0544,S0563,S05A1,S05AS,S0676,S06AC,S06AK,S06UN,S0710,S0715,S0760,S0775&resp=webp&quality=50&BKGND=TRANSPARENT&angle=60&width=750" alt="asd" />
 // let car = {
 //   model: 'X1 Sports Activity Vehicle',
 //   vehicle_code = '21XB',
